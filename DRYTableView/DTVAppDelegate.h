ade02d66f0936cb5c56f4a4abb851e1bcfa3c749
Mike Close <mc@lightmedium.com>
2014-01-09 21:00:48 -0500

    Initial Commit

---
+++ DRYTableView/DTVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTVAppDelegate.h
+//  DRYTableView
+//
+//  Created by C. Michael Close on 1/9/14.
+//  Copyright (c) 2014 LightMedium, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
